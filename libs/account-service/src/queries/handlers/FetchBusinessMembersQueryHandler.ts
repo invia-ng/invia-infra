7fd76dfc99a38a0c1497ef5fe2cabe7e7b3a35ae
tisanyada <tisanyada@gmail.com>
2026-06-20 14:54:18 +0100

    feat: updated FetchBusinessMembersQueryHandler via account-service

--- libs/account-service/src/queries/handlers/FetchBusinessMembersQueryHandler.ts
+++ libs/account-service/src/queries/handlers/FetchBusinessMembersQueryHandler.ts
@@ -1,9 +1,10 @@
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FetchBusinessMembersInfoQuery } from '../impl';
 import { Inject, NotFoundException } from '@nestjs/common';
 import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
 import { Business } from '@app/common/src/models/business.model';
+import { Account } from '@app/common/src/models/account.model';
 import { AppLogger } from 'libs/common/src/logger/logger.service';
 import { BusinessMemberInfo } from '@app/common/src/models/account.model';
 import modelsFormatter from '@app/common/src/middlewares/models.formatter';
@@ -15,6 +16,8 @@ export class FetchBusinessMembersQueryHandler implements IQueryHandler<
 > {
   constructor(
     @Inject('Logger') private readonly logger: AppLogger,
+    @InjectRepository(Account)
+    private readonly accountRepository: Repository<Account>,
     @InjectRepository(Business)
     private readonly businessRepository: Repository<Business>,
   ) {}
@@ -25,21 +28,28 @@ export class FetchBusinessMembersQueryHandler implements IQueryHandler<
 
       const { secureUser } = query;
 
-      const business = await this.businessRepository.findOne({
-        where: [
-          {
-            members: {
+      const account = await this.accountRepository.findOne({
+        where: {
+          id: secureUser.id,
+        },
+        relations: ['business', 'business.members', 'business.account'],
+      });
+
+      let business = account?.business;
+
+      if (!business) {
+        business = await this.businessRepository.findOne({
+          where: {
+            account: {
               id: secureUser.id,
             },
           },
-        ],
-      });
+        });
+      }
 
       if (!business) {
         throw new NotFoundException(`Business record not found for user`);
       }
-
-      console.log('[BUSINESS-INFO] :: ', business);
 
       this.logger.log('[FETCH-BUSINESS-MEMBERS-SUCCESS]');
 
@@ -48,7 +58,7 @@ export class FetchBusinessMembersQueryHandler implements IQueryHandler<
         .filter((member) => member.id !== secureUser.id)
         .map((member) => modelsFormatter.FormatBusinessMemberInfo(member));
     } catch (error) {
-      this.logger.log(`[FETCH-BUSINESS-MEMBERS-HANDLER]: ${error}`);
+      this.logger.error(`[FETCH-BUSINESS-MEMBERS-HANDLER]: ${error}`);
       throw error;
     }
   }
